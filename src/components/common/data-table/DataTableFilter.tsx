0005903322fa0c00e634cea658b65f112f3d8fe1
Azhar Siddiqui <azhartsiddiqui@gmail.com>
2025-01-14 13:21:27 +0530

    feat: month range picker

--- src/components/common/data-table/DataTableFilter.tsx
+++ src/components/common/data-table/DataTableFilter.tsx
@@ -1,11 +1,216 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+import {
+  ChevronLeft,
+  ChevronRight,
+  Download,
+  FileDown,
+  Search,
+  Settings2,
+} from "lucide-react";
 
-const DataTableFilter = (props: Props) => {
-  return (
-    <div>DataTableFilter</div>
-  )
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Table } from "@tanstack/react-table";
+
+interface DataTableFilterProps<TData> {
+  filterKey: string;
+  table: Table<TData>;
 }
 
-export default DataTableFilter
+const DataTableFilter = <TData,>({
+  filterKey,
+  table,
+}: DataTableFilterProps<TData>) => {
+  const columnLength = table
+    .getAllColumns()
+    .filter((col) => col.getIsVisible()).length;
+
+  const handleSelectAllColumns = () => {
+    table.getAllColumns().forEach((column) => {
+      column.toggleVisibility(true);
+    });
+  };
+
+  const handleDeselectAllColumns = () => {
+    const allColumns = table
+      .getAllColumns()
+      .filter((column) => column.getCanHide());
+    const visibleColumns = allColumns.filter((column) => column.getIsVisible());
+
+    // Ensure at least one column remains visible
+    if (visibleColumns.length > 1) {
+      allColumns.forEach((column) => {
+        // Hide all columns except the first visible one
+        if (visibleColumns[0] !== column) {
+          column.toggleVisibility(false);
+        }
+      });
+    }
+
+    // If no columns are visible, ensure the first column becomes visible
+    if (visibleColumns.length === 0) {
+      const firstColumn = allColumns[0];
+      if (firstColumn) {
+        firstColumn.toggleVisibility(true);
+      }
+    }
+  };
+
+  return (
+    <div className="pb-4 flex flex-col sm:flex-row sm:justify-between gap-4">
+      <Input
+        placeholder={`Filter ${filterKey}...`}
+        value={(table.getColumn(filterKey)?.getFilterValue() as string) ?? ""}
+        onChange={(event) =>
+          table.getColumn(filterKey)?.setFilterValue(event.target.value)
+        }
+        className="w-full max-w-lg capitalize"
+        startIcon={<Search className="size-4" />}
+      />
+      <div className="flex gap-2">
+        <div className="flex items-center w-full">
+          <Button variant="link">
+            <ChevronLeft />
+          </Button>
+          <p className="text-primary text-sm font-medium">Nov&apos;24</p>
+          <Button variant="link">
+            <ChevronRight />
+          </Button>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="link"
+              className="w-full ml-auto focus-visible:ring-0 hover:no-underline"
+            >
+              Exports <FileDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            align="center"
+            className="p-2 mx-4 md:mx-auto flex flex-col gap-y-2"
+          >
+            <Button
+              variant="outline"
+              className="group w-full ml-auto focus-visible:ring-0 hover:no-underline flex items-center justify-between px-2 text-primary hover:text-primary border-primary"
+            >
+              Export All Data
+              <Download className="group-hover:animate-bounce" />
+            </Button>
+            <Button
+              variant="outline"
+              className="group w-full ml-auto focus-visible:ring-0 hover:no-underline flex items-center justify-between px-2 text-primary hover:text-primary border-primary"
+            >
+              Export All Vessel Data
+              <Download className="group-hover:animate-bounce" />
+            </Button>
+            <Button
+              variant="outline"
+              className="group w-full ml-auto focus-visible:ring-0 hover:no-underline flex items-center justify-between px-2 text-primary hover:text-primary border-primary"
+            >
+              Export All Voyage Data
+              <Download className="group-hover:animate-bounce" />
+            </Button>
+            <Button
+              variant="outline"
+              className="group w-full ml-auto focus-visible:ring-0 hover:no-underline flex items-center justify-between px-2 text-primary hover:text-primary border-primary"
+            >
+              Export Monthly Vessel Data
+              <Download className="group-hover:animate-bounce" />
+            </Button>
+            <Button
+              variant="outline"
+              className="group w-full ml-auto focus-visible:ring-0 hover:no-underline flex items-center justify-between px-2 text-primary hover:text-primary border-primary"
+            >
+              Export Monthly Voyage Data
+              <Download className="group-hover:animate-bounce" />
+            </Button>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="hidden sm:block">
+              <Button
+                variant="link"
+                className="w-full ml-auto focus-visible:ring-0 hover:no-underline "
+              >
+                Settings <Settings2 />
+              </Button>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            align="end"
+            className="w-72 md:w-96 p-2 mx-4 md:mx-auto"
+          >
+            <div className="flex items-center justify-between">
+              <p className="text-sm font-bold">Show Columns</p>
+              <div className="flex items-center space-x-2">
+                <Button
+                  variant="link"
+                  className="hover:no-underline p-0"
+                  onClick={handleSelectAllColumns}
+                >
+                  Select All
+                </Button>
+                <Separator
+                  orientation="vertical"
+                  className="border border-primary h-4"
+                />
+                <Button
+                  variant="link"
+                  className="hover:no-underline p-0"
+                  onClick={handleDeselectAllColumns}
+                >
+                  Deselect All
+                </Button>
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-2 pb-2">
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => {
+                  return (
+                    <Button
+                      className="capitalize border border-primary backdrop-blur-xl"
+                      key={column.id}
+                      onClick={() => {
+                        if (
+                          columnLength > 1 ||
+                          (columnLength === 1 && !column.getIsVisible())
+                        ) {
+                          column.toggleVisibility(!column.getIsVisible());
+                        } else if (columnLength === 1) {
+                          column.toggleVisibility(true);
+                        }
+                      }}
+                      variant={column.getIsVisible() ? "default" : "outline"}
+                    >
+                      {column.id}
+                    </Button>
+                  );
+                })}
+            </div>
+            <Separator className="my-4" />
+            <div>
+              <p className="text-sm font-bold mb-2">Group by</p>
+              <Button variant="outline" className="border-primary text-primary">
+                None
+              </Button>
+            </div>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+};
+
+export default DataTableFilter;
---
+++ src/components/common/date-pickers/month-range-picker/index.tsx
@@ -0,0 +1,276 @@
+"use client";
+
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ArrowLeft, ArrowRight, CalendarIcon } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import {
+  format,
+  isAfter,
+  isBefore,
+  isFuture,
+  isSameMonth,
+  startOfMonth,
+  subMonths,
+} from "date-fns";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const MonthRangePicker: React.FC = () => {
+  const today = new Date();
+  const defaultEnd = startOfMonth(today);
+  const defaultStart = startOfMonth(subMonths(today, 5));
+
+  const [currentYear, setCurrentYear] = React.useState(today.getFullYear());
+  const [selectedRanges, setSelectedRanges] = React.useState<{
+    start: Date | null;
+    end: Date | null;
+  }>({
+    start: defaultStart,
+    end: defaultEnd,
+  });
+  const [startDate, setStartDate] = React.useState<Date | undefined>(
+    new Date()
+  );
+  const [endDate, setEndDate] = React.useState<Date | undefined>(new Date());
+
+  const handleMonthClick = (monthIndex: number) => {
+    const selectedDate = startOfMonth(new Date(currentYear, monthIndex));
+
+    if (isFuture(selectedDate)) return;
+
+    setSelectedRanges((prev) => {
+      const { start, end } = prev;
+
+      // If both start and end are not set, set start to the selected date
+      if (!start && !end) return { start: selectedDate, end: null };
+
+      // If start is set but end is not, set end and ensure start < end
+      if (start && !end) {
+        return isBefore(selectedDate, start)
+          ? { start: selectedDate, end: start }
+          : { start, end: selectedDate };
+      }
+
+      // If both start and end are set, reset and set start to the selected date
+      return { start: selectedDate, end: null };
+    });
+  };
+
+  const handlePreviousYear = () => setCurrentYear((prev) => prev - 1);
+  const handleNextYear = () => setCurrentYear((prev) => prev + 1);
+
+  const isFutureMonth = (monthIndex: number) =>
+    isFuture(new Date(currentYear, monthIndex, 1));
+
+  const isSelected = (monthIndex: number) => {
+    const { start, end } = selectedRanges;
+    const date = startOfMonth(new Date(currentYear, monthIndex));
+    if (!start) return false;
+
+    if (end) {
+      return isAfter(date, start) && isBefore(date, end);
+    }
+
+    return isSameMonth(date, start);
+  };
+
+  const formatRange = () => {
+    const { start, end } = selectedRanges;
+    if (!start) return "Select Range";
+    if (!end) return `${format(start, "MMM ''yy")} - Select End`;
+    return `${format(start, "MMM ''yy")} - ${format(end, "MMM ''yy")}`;
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className=" rounded-none border-none focus-visible:ring-0 shadow-none hover:bg-white"
+        >
+          Month <span className="text-sky-900">{formatRange()}</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-80 shadow-none p-2" align="start">
+        <DropdownMenuLabel>Show data for</DropdownMenuLabel>
+        <div className="border rounded-md p-2">
+          <div className="flex items-center justify-between">
+            <Button
+              variant="ghost"
+              className="p-2 hover:bg-transparent"
+              onClick={handlePreviousYear}
+            >
+              <ArrowLeft className="size-4" />
+            </Button>
+            <span className="text-sm">{currentYear}</span>
+            <Button
+              variant="ghost"
+              className="p-2 hover:bg-transparent"
+              onClick={handleNextYear}
+              disabled={currentYear >= today.getFullYear()}
+            >
+              <ArrowRight className="size-4" />
+            </Button>
+          </div>
+          <DropdownMenuGroup className="grid grid-cols-3 mt-2">
+            {months.map((month, index) => {
+              const isDisabled = isFutureMonth(index);
+              const isInRange = isSelected(index);
+              const isStart =
+                selectedRanges.start &&
+                isSameMonth(selectedRanges.start, new Date(currentYear, index));
+              const isEnd =
+                selectedRanges.end &&
+                isSameMonth(selectedRanges.end, new Date(currentYear, index));
+
+              return (
+                <Button
+                  key={month}
+                  variant="ghost"
+                  className={`hover:no-underline rounded-none hover:bg-sky-100 ${
+                    isDisabled ? "text-gray-400 cursor-not-allowed" : ""
+                  } ${
+                    isInRange ? "bg-sky-100 text-sky-900 hover:bg-sky-100" : ""
+                  } ${
+                    isStart || isEnd
+                      ? "bg-sky-900 text-white hover:bg-sky-900 hover:text-white"
+                      : ""
+                  }`}
+                  onClick={() => handleMonthClick(index)}
+                  disabled={isFutureMonth(index)}
+                >
+                  {month.slice(0, 3)}
+                </Button>
+              );
+            })}
+          </DropdownMenuGroup>
+        </div>
+        <Separator className="my-4 mb-2" />
+        <DropdownMenuLabel>Show data trend for</DropdownMenuLabel>
+        <div className="flex items-center justify-between gap-x-2">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-full text-left font-normal",
+                  !startDate && "text-muted-foreground"
+                )}
+              >
+                {startDate ? (
+                  format(startDate, "dd-MM-yyyy")
+                ) : (
+                  <span>Pick a date</span>
+                )}
+                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                mode="single"
+                selected={startDate}
+                onSelect={setStartDate}
+                disabled={(date) =>
+                  date > new Date() || date < new Date("1900-01-01")
+                }
+                initialFocus
+                showOutsideDays={false}
+              />
+            </PopoverContent>
+          </Popover>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-full text-left font-normal",
+                  !endDate && "text-muted-foreground"
+                )}
+              >
+                {endDate ? (
+                  format(endDate, "dd-MM-yyyy")
+                ) : (
+                  <span>Pick a date</span>
+                )}
+                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="end">
+              <Calendar
+                mode="single"
+                selected={endDate}
+                onSelect={setEndDate}
+                disabled={(date) =>
+                  date > new Date() || date < new Date("1900-01-01")
+                }
+                initialFocus
+                showOutsideDays={false}
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div className="mt-4 grid grid-cols-2 gap-2">
+          <Button className="w-full">Last 1 month</Button>
+          <Button variant="outline" className="w-full">
+            Last 3 month
+          </Button>
+          <Button variant="outline" className="w-full">
+            Last 6 month
+          </Button>
+          <Button variant="outline" className="w-full">
+            Last 9 month
+          </Button>
+          <Button variant="outline" className="w-full">
+            Last 12 month
+          </Button>
+          <Button variant="outline" className="w-full">
+            Last 18 month
+          </Button>
+        </div>
+        <Separator className="my-4" />
+        <div className="flex items-center justify-between">
+          <Button
+            variant="destructive"
+            onClick={() => setSelectedRanges({ start: null, end: null })}
+          >
+            Reset Changes
+          </Button>
+          <Button onClick={() => console.log(selectedRanges)}>
+            Save Selection
+          </Button>
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default MonthRangePicker;
